e4c65b66438f8a1a015a4a564af21b6cafd258bb
neupane11 <sandip.neupane11>
2021-05-05 21:09:55 -0700

    working on cornell movie data

---
+++ FinalProject/deep_learning_chatbot/src/app.py
@@ -0,0 +1,4 @@
+lines=open('../dataset/movie_lines.txt',encoding='utf-8',errors='ignore').read().split('\n')
+conversations=open('../dataset/movie_conversations.txt',encoding='utf-8',errors='ignore').read().split('\n')
+
+#pre-process the data
